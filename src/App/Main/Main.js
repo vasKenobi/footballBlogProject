86a5936c2922f554429a1f45eb42ae5fdc8d8c80
Vasyl Kle <vasyl.klets@gmail.com>
2019-11-27 13:49:38 +0200

    first section component added

--- src/App/Main/Main.js
+++ src/App/Main/Main.js
@@ -1,63 +1,11 @@
 import React from 'react'
+import Firstsection from './Firstsection/Firstsection'
 
 
 const Main = () => {
     return (
       <div>
-
-<section className="section first-section">
-            <div className="container-fluid">
-                <div className="masonry-blog clearfix">
-                    <div className="left-side">
-                        <div className="masonry-box post-media">
-                             <img src="images/EPL/LFC_MC_Preview.png" alt="" className="img-fluid"/>
-                             <div className="shadoweffect">
-                                <div className="shadow-desc">
-                                    <div className="blog-meta">
-                                        <span className="bg-aqua"><a href="blog-football-category-EPL.html" title="">English Premier League</a></span>
-                                        <h4><a href="blog-football-EPL.html" title="">No Salah, Firmino or Mane in Liverpool and Manchester City combined XI </a></h4>
-                                        <small><a href="blog-football-EPL.html" title="">08 November, 2019</a></small>
-                                        <small><a href="#" title="">by Gzhegozh Bzhezh</a></small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="center-side">
-                        <div className="masonry-box post-media">
-                             <img src="images/MSL/lodeiro.png" alt="" className="img-fluid"/>
-                             <div className="shadoweffect">
-                                <div className="shadow-desc">
-                                    <div className="blog-meta">
-                                        <span className="bg-aqua"><a href="blog-football-category-MSL.html" title="">Major Soccer League</a></span>
-                                        <h4><a href="blog-football-MSL.html" title="">Lodeiro vs Pozuelo: The creative pair central as Seattle meet Toronto</a></h4>
-                                        <small><a href="blog-football-MSL.html" title="">9 November, 2019</a></small>
-                                        <small><a href="#" title="">Gzhegozh Bzhezh</a></small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="right-side hidden-md-down">
-                        <div className="masonry-box post-media">
-                             <img src="images/bl/BMteam.png" alt="" className="img-fluid"/>
-                             <div className="shadoweffect">
-                                <div className="shadow-desc">
-                                    <div className="blog-meta">
-                                        <span className="bg-aqua"><a href="blog-football-category-BL.html" title="">English Premier League</a></span>
-                                        <h4><a href="blog-football-BL.html" title="">What next for Bayern following Kovac sacking?</a></h4>
-                                        <small><a href="blog-football-BL.html" title="">05 November, 2019</a></small>
-                                        <small><a href="#" title="">by Gzhegozh</a></small>
-                                    </div>
-                                </div>
-                             </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+          <Firstsection/>
 
         <section className="section wb">
             <div className="container">
