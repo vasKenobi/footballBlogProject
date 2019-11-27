2260d1c595982c3b4aa5549088c6fc3d03051fc9
Vasyl Kle <vasyl.klets@gmail.com>
2019-11-27 13:03:36 +0200

    created Header,Main,Footer components

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ import './css/responsive.css'
 import './css/style.css'
 import './css/version/blog-football.css'
 
-
-const App = () => {
+const Header = () => {
 	return (
     <div>
-        <div className="collapse top-search" id="collapseExample">
+
+    <div className="collapse top-search" id="collapseExample">
           <div className="card card-block">
             <div className="newsletter-widget text-center">
                 <form className="form-inline">
@@ -89,7 +89,17 @@ const App = () => {
             </div>
         </header>
 
-        <section className="section first-section">
+        </div>
+
+    
+    )
+  }
+  
+  const Main = () => {
+    return (
+      <div>
+
+<section className="section first-section">
             <div className="container-fluid">
                 <div className="masonry-blog clearfix">
                     <div className="left-side">
@@ -413,6 +423,63 @@ const App = () => {
                 </div>
             </div>
         </section>
+
+
+      </div>
+
+    )
+  }  
+
+  const Footer = () => {
+    return (
+      <footer class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2">
+                        <div class="widget">
+                            <div class="footer-text text-center">
+                                <a href="blog-football-index.html"><img src="images/version/blog-football-footer-logo.png" alt="" class="img-fluid"/></a>
+                                <p>Football blog - last news, interesting articles, forecasts.</p>
+                                <div class="social">
+                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a>              
+                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a>
+                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Instagram"><i class="fa fa-instagram"></i></a>
+                                    <a href="#" data-toggle="tooltip" data-placement="bottom" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                                </div>
+
+                                <hr class="invis"/>
+
+                                <div class="newsletter-widget text-center">
+                                    <form class="form-inline">
+                                        <input type="text" class="form-control" placeholder="Enter your email address"/>
+                                        <button type="submit" class="btn btn-primary">Subscribe <i class="fa fa-envelope-open-o"></i></button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <br/>
+                        <br/>
+                        <div class="copyright">&copy; Footbal Blog.</div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+  }
+  
+
+
+const App = () => {
+	return (
+    <div>
+        <Header/>
+			  <Main/>
+			  <Footer/>
 
   </div>
   )
