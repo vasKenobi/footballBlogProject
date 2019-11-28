015eebe5d7fe0e1c45f72d16ddb3d9a5162f9e04
Vasyl Kle <vasyl.klets@gmail.com>
2019-11-28 15:45:07 +0200

    rewrote PostListItem to class component

--- src/App/Main/Mainsection/Postlist/PostListItem.js
+++ src/App/Main/Mainsection/Postlist/PostListItem.js
@@ -1,24 +1,18 @@
-import React from 'react'
+import React,{Component} from 'react'
 import PropTypes from 'prop-types';
 
 
 
 
-const PostListItem = ({
-    category,
-    title,
-    description,
-    date,
-    author,
-    image,
-}) => {
+class PostListItem extends Component {
+  render(){
 
     return (
                                 <div className="blog-box row">
                                     <div className="col-md-4">
                                         <div className="post-media">
                                             <a href="blog-football-EPL.html" title="">
-                                                <img src={image} alt="" className="img-fluid"/>
+                                                <img src={this.props.image} alt="" className="img-fluid"/>
                                                 <div className="hovereffect"></div>
                                             </a>
                                         </div>
@@ -26,23 +20,18 @@ const PostListItem = ({
 
                                     <div className="blog-meta big-meta col-md-8">
                                     
-                                        <span className="bg-aqua"><a href="blog-football-category-EPL.html" title="">{category}</a></span>
-                                        <h4><a href="blog-football-EPL.html" title="">{title}</a></h4>
-                                        <p>{description}</p>
+                                        <span className="bg-aqua"><a href="blog-football-category-EPL.html" title="">{this.props.category}</a></span>
+                                        <h4><a href="blog-football-EPL.html" title="">{this.props.title}</a></h4>
+                                        <p>{this.props.description}</p>
                                      
-                                        <small><a href="blog-football-EPL.html" title="">{date}</a></small>
-                                        <small><a href="#" title="">{author}</a></small>
+                                        <small><a href="blog-football-EPL.html" title="">{this.props.date}</a></small>
+                                        <small><a href="#" title="">{this.props.author}</a></small>
                                         
                                     </div>
                                 </div>
-
-                                
-
-                                  
-
-      
-
-    )
+        )
+                            
+    }
   }
 
   PostListItem.propTypes = {
